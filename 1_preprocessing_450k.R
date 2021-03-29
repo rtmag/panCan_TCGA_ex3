bd7dfcf56b08bb64fb7786f0658487b99619c12e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2021-03-29 22:21:32 +0800

    Create 1_preprocessing_450k.R

---
+++ 1_preprocessing_450k.R
@@ -0,0 +1,9 @@
+setwd("/root/TCGA/Rnbeads")
+suppressMessages(library(RnBeads))
+options(bitmapType="cairo")
+options(scipen=999)
+suppressMessages(library(IlluminaHumanMethylation450kmanifest))
+
+
+
+  idat.dir <- file.path("/root/TCGA/tcgaBiolink")
