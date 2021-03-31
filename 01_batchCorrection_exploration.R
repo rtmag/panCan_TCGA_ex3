0f218c70c02670672009778285da3a564aa19a7b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2021-03-31 21:32:25 +0800

    Update 01_batchCorrection_exploration.R

--- 01_batchCorrection_exploration.R
+++ 01_batchCorrection_exploration.R
@@ -197,7 +197,76 @@ i = 13 #KIRP
   #Normalization and background correction
   rnb.set.norm <- rnb.execute.normalization(rnb.set.sampleRMV, method="swan",bgcorr.method="methylumi.noob")
 
+  # write rnb.set.norm
+  save.rnb.set(rnb.set.norm,
+               path=paste("/root/TCGA/Rnbeads/",projects[i],"/","RnBeads_normalization/rnb.set.norm_withNormal.RData",sep=""))
 
+  # write beta
+  meth.norm<-meth(rnb.set.norm,row.names=T)
+  saveRDS(meth.norm, paste(projects[i],"/","RnBeads_normalization/betaVALUES_withNormal.rds",sep=""))
+
+  # write mval
+  mval.norm <- mval(rnb.set.norm,row.names=T)
+  saveRDS(mval.norm, paste(projects[i],"/","RnBeads_normalization/mVALUES_withNormal.rds",sep=""))
+
+  #### CPACOR
+  # extract control probe intensities(exluding negative control probes)
+  qc_data<-qc(rnb.set.norm)
+  # Remove the 614 negative control probes from the intensity
+  control.annotation <- rnb.get.annotation("controls450")
+  non_neg_ctr_prob <- rownames(control.annotation[control.annotation$Target!="NEGATIVE",])
+  # add green intensity for non-negativeControl probes
+  qc_data_ctr_noNeg <- qc_data[[1]][rownames(qc_data[[1]]) %in% non_neg_ctr_prob,]
+  rownames(qc_data_ctr_noNeg) <- gsub("$","_green",rownames(qc_data_ctr_noNeg),perl=TRUE) #change name to avoid conflict
+  # add red intensity for non-negativeControl probes
+  qc_data_ctr_noNeg <- rbind(qc_data_ctr_noNeg, qc_data[[2]][rownames(qc_data[[2]]) %in% non_neg_ctr_prob,])
+  # remove rows with NAs
+  qc_data_ctr_noNeg <- qc_data_ctr_noNeg[complete.cases(qc_data_ctr_noNeg), ]
+
+  #PCA of control-probe intensities
+  pca <- prcomp(na.omit(t(qc_data_ctr_noNeg)))
+  ctrlprobes.scores = pca$x
+  colnames(ctrlprobes.scores) = paste(colnames(ctrlprobes.scores), '_cp', sep='')
+  phe=as.data.frame(ctrlprobes.scores)
+  saveRDS(phe, paste(projects[i],"/","RnBeads_normalization/PC30_controProbeIntensity.rds",sep=""))
+         
+  lfla=as.formula('beta[i, ] ~  phe$PC1_cp + phe$PC2_cp + 
+    phe$PC3_cp + phe$PC4_cp + phe$PC5_cp + phe$PC6_cp + phe$PC7_cp + 
+    phe$PC8_cp + phe$PC9_cp + phe$PC10_cp + phe$PC11_cp + phe$PC12_cp + 
+    phe$PC13_cp + phe$PC14_cp + phe$PC15_cp + phe$PC16_cp + phe$PC17_cp + 
+    phe$PC18_cp + phe$PC19_cp + phe$PC20_cp + phe$PC21_cp + phe$PC22_cp + 
+    phe$PC23_cp + phe$PC24_cp + phe$PC25_cp + phe$PC26_cp + phe$PC27_cp + 
+    phe$PC28_cp + phe$PC29_cp + phe$PC30_cp')
+
+  # regression
+  beta = meth.norm
+  samples=colnames(beta)
+  nvar = nrow(beta)
+  res=matrix(ncol=ncol(beta), nrow=nrow(beta))
+  rownames(res)=rownames(beta)
+  colnames(res)=colnames(beta)
+  for(i in 1:nvar) {
+	tryCatch({fit = lm(lfla,na.action=na.exclude)}, error = function(error) {return(NA)})
+	if(!exists("fit")){
+		res[i,colnames(as.matrix(beta))] = rep(NA, length(colnames(as.matrix(beta))))
+	}else{
+		res[i,rownames(as.matrix(fit$residuals))] = fit$residuals
+       		rm(fit)
+	}		
+  }
+          
+  # PCA on the resulting regression residuals (excluding markers with missing data) and include PC 1 to 5 as linear predictors in the final regression model.
+  pca <- prcomp(t(na.omit(res)))
+  dim(pca$x)
+  pca.scores = pca$x[,1:30]
+  saveRDS(meth.norm, paste(projects[i],"/","RnBeads_normalization/PC5_resultingResiduals.rds",sep=""))
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 #   485577 probes
 #        of which: 482421 CpG, 3091 CpH, and 65 rs
 #Region types:
